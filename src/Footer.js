07548d8f3e52a7b6a1175d935863e7c1cb94254d
iftekhs <iftekhstudio@gmail.com>
2022-07-21 20:31:21 +0600

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card, CardContent, Typography } from "@material-ui/core";
+
+import "./Footer.css"
+
+function Footer() {
+    return (
+      
+      
+      <div className="footer">
+       
+       
+       <h1 className="safety"> 1. Wear Mask 😷</h1>
+       <h1 className="safety"> 2. Wash your Hands 🧼 </h1>
+       <h1 className="safety"> 3. Follow health safety 👨‍⚕️</h1>
+       
+       </div>
+       
+       
+       
+    )
+}
+
+export default Footer
